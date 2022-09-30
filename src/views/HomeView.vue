c3e94795f6ba515b1b8384d909b88af233c3a7e3
guandexing <guandexis@gmail.com>
2022-09-30 15:39:35 +0800

    project add

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+import { reactive, computed } from 'vue'
+
+interface Book {
+  title: String,
+  year?: number
+}
+const book: Book = reactive({ title: '鲁本' })
+
+
+const comBook = computed(()=>{
+  return book.title  === '本'? 'Yes': 'No'
+})
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    {{ book.title }}
+    {{comBook}}
+  </main>
+</template>
